10e5863590ec67f7192b7f803b429451c4d213a7
CE-Automaton <jrj20070301@163.com>
2024-01-09 19:21:08 +0800

    update

--- 2023年/1-30/CQYC-C3讲溶解嗨/gqygd.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-	freopen("gqygd.in", "r", stdin);
-	freopen("gqygd.out", "w", stdout);
-	cout<<"1\n";
-	return 0;
-} 
--- 2023年/2-27/CQYC÷3讲溶解呐/race/race.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-	freopen("race.in", "r", stdin); 
-	freopen("race.out", "w", stdout);
-	puts("-1");
-	return 0;
-}
