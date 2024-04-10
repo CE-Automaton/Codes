74e3436ce549e884440cf7f91b8fe1c4f3f16c64
CE-Automaton <jrj20070301@163.com>
2024-04-10 09:40:00 +0800

    qwq

---
+++ 2024年/4-6出的t2/t1/std.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define rep(x,y,z) for(int x=(y);x<=(z);x++)
+#define repn(x) rep(x,1,n)
+inline int read(){int s=0,w=1;char c=getchar();while(c<48||c>57) {if(c=='-') w=-1;c=getchar();}while(c>=48&&c<=57)s=(s<<1)+(s<<3)+c-48,c=getchar();return s*w;}
+using namespace std;
+const int N=3e5+5;
+int n,t[N<<1],a[N];
+void add_(int x,int k){
+	x++;
+	while(x<=(n<<1))t[x]=max(t[x],k),x+=x&-x;
+}
+int query(int x){
+	x++;
+	int ans=0;
+	while(x)ans=max(ans,t[x]),x-=x&-x;
+	return ans; 
+}
+int main(){
+	freopen(".in","r",stdin);
+	freopen(".out","w",stdout);
+	n=read();
+	repn(i)a[i]=read();
+	repn(i){
+		int w=a[i]-i;
+		if(w<0)continue;
+		int k=query(w);
+		add_(w,k+1);
+		if(i==n){
+			cout <<n-(k+1);
+			return 0;
+		}
+	}
+	return 0;
+}
---
+++ 2024年/4-6出的t2/t1/subtask.md
@@ -0,0 +1,7 @@
+Subtask 1（10 pts）：$n\le 5$。 
+
+Subtask 2（15 pts）：$n\le 200$。 
+
+Subtask 3（25 pts）：$n\le 2000$。 
+
+Subtask 4（50 pts）：无特殊限制。
