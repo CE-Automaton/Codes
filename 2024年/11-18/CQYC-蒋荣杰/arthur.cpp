2946167399f07ae6e26ad806f50085e63c6ac507
CE-Automaton <jrj20070301@163.com>
2024-11-27 22:10:26 +0800

    qwq

---
+++ 2024年/11-18/CQYC-蒋荣杰/arthur.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define File(xxx) freopen(xxx".in","r",stdin),freopen(xxx".out","w",stdout)
+using namespace std;
+typedef long long LL;
+int n, u, v;
+LL ans;
+
+template <typename T> inline void debug(T x) { cerr<<x; }
+template <typename T, typename ...T_> inline void debug(T x, T_ ...p) { cerr<<x<<' ', debug(p...); }
+template <typename T> void read(T& x) {
+	x = 0; int f = 0; char c = getchar();
+	while(c < '0' || c > '9') f |= (c == '-'), c=getchar();
+	while(c >= '0' && c <= '9') x=(x<<1)+(x<<3)+(c^48), c=getchar();
+	x=(f ? -x : x);
+}
+int lne; char put[105];
+template <typename T> void write(T x, char ch) {
+	lne = 0; if(x < 0) putchar('-'), x=-x;
+	do { put[++lne]=x%10, x/=10; } while(x);
+	while(lne) putchar(put[lne--]^48);
+	putchar(ch);
+}
+
+signed main() {
+	File("arthur");
+	read(n);
+	for(int i = 1; i <= n; ++i)
+		ans=ans+1LL*i*(n-i+1);
+	// write(ans, '\n');
+	for(int i = 2; i <= n; ++i) {
+		read(u), read(v);
+		if(u > v) swap(u, v);
+		ans=ans-1LL*u*(n-v+1);
+	}
+	write(ans, '\n');
+	return 0;
+}
